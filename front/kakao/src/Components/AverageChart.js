7099164407d5183b33aec48dc52dac958dad9150
yabby <yabby1997@gmail.com>
2021-01-20 12:46:16 +0900

    [refactor/front] AverageChart 컴포넌트 리팩터 하며 average데이터 형식 변경. - #6

--- front/kakao/src/Components/AverageChart.js
+++ front/kakao/src/Components/AverageChart.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import { XYPlot, Crosshair, XAxis, YAxis, VerticalBarSeries, HorizontalGridLines, DiscreteColorLegend } from 'react-vis';
 import '../../node_modules/react-vis/dist/style.css';
 import Card from "@material-ui/core/Card"
@@ -8,18 +8,17 @@ import { makeStyles } from "@material-ui/core/styles"
 const useStyles = makeStyles(theme => ({
     normal: {
         padding: theme.spacing(0.7),
-        color: "#fff",
-        textAlign: "center",
-        minWidth: 100
+        minWidth: 90
     },
-    title: {
+    time: {
+        textAlign: "center",
         fontSize: 13,
         fontWeight: 700
     },
     content: {
         textAlign: "left",
         fontSize: 13,
-        fontWeight: 500
+        fontWeight: 700
     },
     legend: {
         position: 'absolute', 
@@ -42,18 +41,19 @@ const useStyles = makeStyles(theme => ({
 
 function AverageChart(props){
     const classes = useStyles()
-    const averageData = [props.averageData.masked, props.averageData.unmasked];
+    const averageData = props.averageData;
     const maskedColor = "#7abd91"
     const unmaskedColor = "#ff6962"
     const charWidth = props.width;
     const chartHeight = 250;
+    const barWidth = 0.3;
     const gridColor = '#DFE2E6'
     const [crosshairValues, setCrosshairValues] = useState([]);
     const [targetTime, setTime] = useState("");
     const [targetMasked, setMasked] = useState(0);
     const [targetUnmasked, setUnmasked] = useState(0);
-    const [targetRatio, setRatio] = useState(0);
-
+    const [targetRatio, setRatio] = useState("");
+    const tick = ["0600", "0700", "0800", "0900", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000", "2100"]
     const legend = [
         {
             title: ' 마스크 착용',
@@ -63,30 +63,31 @@ function AverageChart(props){
             title: ' 마스크 미착용',
             color: unmaskedColor,
         }
-    ]
-
-    useEffect(() => {
-        if(crosshairValues.length === 0){
-            console.log("empty!!")
-        }
-        else{
-            setTime(Object.values(crosshairValues[0])[1]); 
-            setMasked(Object.values(crosshairValues[0])[0]);
-            setUnmasked(Object.values(crosshairValues[1])[0]);
-            setRatio(targetMasked / (targetMasked + targetUnmasked));
-        }
-    }, [crosshairValues, targetMasked, targetUnmasked]);
-
+    ]         
     const _onMouseLeave = () => {
         setCrosshairValues([]);
     };
 
     const _onNearestX = (value, {index}) => {
         setCrosshairValues(averageData.map(d => d[index]));
+        setTime(Object.values(averageData[0][index])[1]);
+        setMasked(Object.values(averageData[0][index])[0]);
+        setUnmasked(Object.values(averageData[1][index])[0]);
+        setRatio(targetMasked / (targetMasked + targetUnmasked));
     };
 
-    const getColor = (value) => {
-        return ["hsl(", (value * 100 + 10).toString(10), ",80%,50%)"].join("");
+    const getCardColor = (maskedRatio) => {
+        if(maskedRatio >= 0.5)
+            return "hsl(" + String(maskedRatio * 240 - 120) + ",80%,50%)";
+        else
+            return "hsl(0, 80%, 50%)";
+    }
+
+    const getFontColor = (maskedRatio) => {
+        if(maskedRatio >= 0.7)
+            return "#2E2E2E";
+        else
+            return "#FFFFFF";
     }
 
     return (
@@ -99,18 +100,18 @@ function AverageChart(props){
         >
             <XAxis 
                 className={classes.xaxis}
-                tickValues={["0600", "0700", "0800", "0900", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000", "2100"]}
+                tickValues={tick}
             />
             <YAxis className={classes.yaxis}/>
             <HorizontalGridLines style={{stroke: gridColor}} />
             <VerticalBarSeries
                 onNearestX={_onNearestX}
-                barWidth={0.3}
+                barWidth={barWidth}
                 data={averageData[0]}
                 color={maskedColor}
             />
             <VerticalBarSeries
-                barWidth={0.3}
+                barWidth={barWidth}
                 data={averageData[1]}
                 color={unmaskedColor}
             />
@@ -123,9 +124,9 @@ function AverageChart(props){
                 <div>
                     <Card 
                         className={classes.normal}
-                        style={{ backgroundColor: getColor(targetRatio), color: '#FFFFFF'
+                        style={{ backgroundColor: getCardColor(targetRatio), color: getFontColor(targetRatio)
                     }}>
-                        <Typography className={classes.title} component="p">{targetTime}시</Typography>
+                        <Typography className={classes.time} component="p">{targetTime}시</Typography>
                         <Typography className={classes.content} component="p">착용 : {targetMasked} 명</Typography>
                         <Typography className={classes.content} component="p">미착용 : {targetUnmasked} 명</Typography>
                     </Card>
