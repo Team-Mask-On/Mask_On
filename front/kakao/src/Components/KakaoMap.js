967e1ea9deffb557954319c5f87c2c57155bc973
yabby <yabby1997@gmail.com>
2021-01-21 11:32:56 +0900

    [feat/front] kakaomap에서 최초 로딩시 좌표를 받아와 설정하는 기능 추가. - #3

--- front/kakao/src/Components/KakaoMap.js
+++ front/kakao/src/Components/KakaoMap.js
@@ -5,8 +5,16 @@ import Sensor from './Sensor';
 
 function KakaoMap(){
     const [sensorData, setSensorData] = useState([])
+    const [latitude, setLatitude] = useState(36.360649863349586)
+    const [longitude, setLongitude] = useState(127.34453802638934)
 
     useEffect(() => {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            console.log("Latitude is :", position.coords.latitude);
+            console.log("Longitude is :", position.coords.longitude);
+            setLatitude(position.coords.latitude)
+            setLongitude(position.coords.longitude)
+        });
         fetchSensors();
     }, [])
 
@@ -18,7 +26,7 @@ function KakaoMap(){
     return(
         <React.Fragment>
             <Map options={{
-                center: new kakao.maps.LatLng(36.360649863349586, 127.34453802638934),
+                center: new kakao.maps.LatLng(latitude, longitude),
                 mapTypeId: kakao.maps.MapTypeId.ROADMAP,
                 maxLevel: 5,
                 minLevel: 2
