e2fafec513d1bcf7aef882da8008bba7e067ca0f
yabby <yabby1997@gmail.com>
2021-01-22 01:37:42 +0900

    [refactor/front] 노티피케이션 애니메이션 변경, 메시지 변경. - #3

--- front/kakao/src/Components/KakaoMap.js
+++ front/kakao/src/Components/KakaoMap.js
@@ -1,8 +1,7 @@
 /*global kakao*/
 import { Map } from 'react-kakao-maps'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Sensor from './Sensor';
-import SearchBar from './SearchBar';
 import { store } from 'react-notifications-component';
 
 function KakaoMap(){
@@ -10,10 +9,25 @@ function KakaoMap(){
     const [latitude, setLatitude] = useState(37.40213319610438)
     const [longitude, setLongitude] = useState(127.10863508204353)
 
-    useEffect(() => {
-        moveToCurrentLocation()
-        fetchSensors();
+    const moveToCurrentLocation = useCallback(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    console.log("getLocation", position.coords.latitude, position.coords.longitude)
+                    moveTo(position.coords.latitude, position.coords.longitude)
+                    message("현위치 좌표 설정 성공!", "현위치 기반으로 좌표가 설정되었습니다.", "success")
+                }
+            )
+        }
+        else{
+            message("현위치 좌표 설정 실패!", "기본값으로 좌표가 설정되었습니다.", "danger")
+        }
     }, [])
+
+    useEffect(() => {
+        moveToCurrentLocation();
+        fetchSensors();
+    }, [moveToCurrentLocation])
 
     const fetchSensors = () => {
         setSensorData(require("../Dummies/sensors.json"));
@@ -26,31 +40,14 @@ function KakaoMap(){
             message: message,
             type: type,
             insert: "top",
-            container: "top-right",
-            animationIn: ["animate__animated animate__fadeIn"],
-            animationOut: ["animate__animated animate__fadeOut"],
-            dismiss: {
-              duration: 3000,
-              onScreen: true
-            }
+            container: "top-right",  
+            animationIn: ["animated", "fadeIn"],
+            animationOut: ["animated", "fadeOut"],
+            dismiss: { duration: 5000 },
+            dismissable: { click: true }
         });
     }
 
-    const moveToCurrentLocation = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                position => {
-                    console.log("getLocation", position.coords.latitude, position.coords.longitude)
-                    moveTo(position.coords.latitude, position.coords.longitude)
-                    message("성공!", "현위치 기반으로 좌표가 재설정 설정되었습니다.", "success")
-                }
-            )
-        }
-        else{
-            message("실패!", "기본값으로 좌표가 설정되었습니다.", "danger")
-        }
-    }
-    
     const moveTo = (latitude, longitude) => {
         setLatitude(latitude)
         setLongitude(longitude)
@@ -58,11 +55,10 @@ function KakaoMap(){
 
     return(
         <React.Fragment>
-            <SearchBar />
             <Map options={{
                 center: new kakao.maps.LatLng(latitude, longitude),
                 mapTypeId: kakao.maps.MapTypeId.ROADMAP,
-                maxLevel: 8,
+                maxLevel: 6,
                 minLevel: 2
             }}>{sensorData.map(sensor => {
                     return <Sensor 
