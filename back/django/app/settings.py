904383a8e06d7b43277d65ffdf99415673cc17f0
SeungWookHan <hswook12@me.com>
2021-01-28 22:34:32 +0900

    [fix/deploy] RDS 연동 완료

--- back/django/app/settings.py
+++ back/django/app/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import json
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0yq!juwbw^ubjgw#ety+5a46hf#l5gw#)6bhq&%306hb2#rr2%'
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -100,10 +112,13 @@ else:
         "default": {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'maskon',
-            'USER': 'root',
-            'PASSWORD': 'rootpassword',
-            'HOST': 'mysql_db',
+            'USER': get_secret("DB_USER"),
+            'PASSWORD': get_secret("DB_PASSWORD"),
+            'HOST': get_secret("DB_HOST"),
             'PORT': '3306',
+            'OPTIONS': {
+                'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+            }
         }
     }
 
