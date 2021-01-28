f3324d1da738e7bc419e9401a27cd7617ef4bccb
yabby <yabby1997@gmail.com>
2021-01-28 22:54:02 +0900

    [feat/back] average log에서 결측치는 0으로 내보내도록 views.py를 수정

--- back/django/log/views.py
+++ back/django/log/views.py
@@ -19,16 +19,35 @@ class SensorLogView(APIView):
 class AverageLogView(APIView):
     def get(self, request, sensor_id):
         average_logs = AverageLog.objects.filter(sensor_id=sensor_id).order_by('average_time')
+        fromTime = 600
+        toTime = 2100
         if average_logs:
             data = {
                 "masked": [],
                 "unmasked":[]
             }
             for log in average_logs:
+                currentTime = int(log.average_time)
+                while currentTime > fromTime:
+                    data["masked"].append({"y": 0, "x":"{:04d}".format(fromTime)})
+                    data["unmasked"].append({"y": 0, "x":"{:04d}".format(fromTime)})
+                    fromTime = fromTime + 5
+                    if (fromTime - 60) % 100 == 0:
+                        fromTime = fromTime + 40
+
                 data["masked"].append({"y":log.average_masked, "x":log.average_time})
                 data["unmasked"].append({"y":log.average_unmasked, "x":log.average_time})
-                print(log.average_time, log.average_masked)
+                fromTime = currentTime
+                print(int(log.average_time), log.average_masked)
                 print(log.average_time, log.average_unmasked)
+            
+            while fromTime <= toTime:
+                data["masked"].append({"y": 0, "x":"{:04d}".format(fromTime)})
+                data["unmasked"].append({"y": 0, "x":"{:04d}".format(fromTime)})
+                fromTime = fromTime + 5
+                if (fromTime - 60) % 100 == 0:
+                    fromTime = fromTime + 40
+
             # avg_serializer = AverageLogSerializer(average_logs, many=True)
             return Response(data, status=status.HTTP_200_OK)
         else:
